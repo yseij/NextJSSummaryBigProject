925c33747dd77d49c52fc1c27d6d4ea0cd826e30
yseij <r0790156@student.thomasmore.be>
2022-02-10 20:13:43 +0100

    Aangepaste componenten en styling toevoegen met CSS-modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  <MeetupDetail
+    image="https://media.gettyimages.com/photos/hallstatt-village-and-hallstatter-see-lake-in-austria-picture-id1150609477?s=612x612"
+    title="first meetup"
+    address="some street"
+    description="this is my first meetup"
+  />;
+}
+
+export default MeetupDetails;
