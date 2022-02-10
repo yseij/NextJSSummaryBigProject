9910df7ccb76d9c8045b7ba76448b5d017157fa2
yseij <r0790156@student.thomasmore.be>
2022-02-10 19:37:57 +0100

    Een lijst met (dummy)meetups weergeven

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First meetup",
+    image:
+      "https://media.gettyimages.com/photos/hallstatt-village-and-hallstatter-see-lake-in-austria-picture-id1150609477?s=612x612",
+    address: "some address 21, Hamont",
+    description: "First meetup yeah",
+  },
+  {
+    id: "m2",
+    title: "Second meetup",
+    image:
+      "https://media.gettyimages.com/photos/hallstatt-village-and-hallstatter-see-lake-in-austria-picture-id1150609477?s=612x612",
+    address: "some address 22, Hamont",
+    description: "Second meetup yeah",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
