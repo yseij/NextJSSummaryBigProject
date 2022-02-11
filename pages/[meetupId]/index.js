ddb45475e4d36a903bfad401102944b79c286a85
yseij <r0790156@student.thomasmore.be>
2022-02-11 12:32:32 +0100

    Werken met dynamische padparameters in getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,22 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://media.gettyimages.com/photos/hallstatt-village-and-hallstatter-see-lake-in-austria-picture-id1150609477?s=612x612",
+        id: meetupId,
+        title: "first meetup",
+        address: "some street",
+        description: "this is my first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
